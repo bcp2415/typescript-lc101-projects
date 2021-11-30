4b2662ca3849734c847c8c70cac503587fbd2a25
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-11-30 10:45:08 -0600

    adds sumMass method to Rocket class

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,1 +1,22 @@
-export class Rocket {}
+import { Payload } from './Payload';
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass( items: Payload[]): number {
+        let total = 0;
+        for (let i = 0; i < items.length; i++) {
+            total += items[i].massKg;
+        }
+        return total;
+    }
+}
